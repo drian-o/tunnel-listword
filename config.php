16f10974de1752a85b22c360ad5ec6b2cafb1bbc
ADRIANSYAH <109329296+drian-o@users.noreply.github.com>
2025-03-24 05:26:08 +0700

    config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'			=> 'your site', // domain input
+	'folder'  		=> 'folder.txt', // this folder brand you want
+	'template'  	=> 'temp.html', // this tamplate html
+	'keywords'  	=> 'kw.txt',  //this folder brand you want
+	'sitemap'		=> 'sitemap.xml', //ganerate sitemap otomatis
+);
+?>
